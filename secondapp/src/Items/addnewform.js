4fc205a7f8094f2a721e0045203d342e30f81fac
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-08-17 18:13:44 +0330

    Update addnewform.js

--- secondapp/src/Items/addnewform.js
+++ secondapp/src/Items/addnewform.js
@@ -1,4 +1,4 @@
-import {useState, useContext, useEffect} from "react";
+import {useState, useContext} from "react";
 import MyContext from "./itemsContext";
 const Form = () => {
   const {items, setItems} = useContext(MyContext)
